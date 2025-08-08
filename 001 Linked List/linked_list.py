9c92cd67830ab135e993ed85b0917baccf991831
akramfdk <akramjeet.singh@gmail.com>
2025-08-09 09:35:06 +1000

    added more linked list methods get, set, insert, remove

--- 001 Linked List/linked_list.py
+++ 001 Linked List/linked_list.py
@@ -83,41 +83,111 @@ class LinkedList:
         self.length -= 1
         return temp
     
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+
+        for _ in range(index):
+            temp = temp.next
+
+        return temp
+        
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+
+        if temp:
+            temp.value = value
+            return True
+        
+        return False
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        pre = self.get(index - 1)
+
+        new_node.next = pre.next
+        pre.next = new_node
+
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length-1:
+            return self.pop()
+        
+        pre = self.get(index-1)
+        curr = pre.next
+
+        pre.next = curr.next
+        curr.next = None
+
+        self.length -= 1
+        return curr
+    
 
 ll = LinkedList()
 
 ll.append(5)
-ll.print_list()
-
 ll.append(3)
+ll.append(8)
 ll.print_list()
 
-ll.pop()
-ll.print_list()
 
-ll.pop()
-ll.print_list()
+# print(ll.remove(-1))
+# ll.print_list()
 
-ll.pop()
-ll.print_list()
+# print(ll.remove(3))
+# ll.print_list()
 
-ll.prepend(5)
-ll.print_list()
+# print(ll.remove(2).value)
+# ll.print_list()
 
-ll.prepend(7)
-ll.print_list()
+# print(ll.remove(1).value)
+# ll.print_list()
 
-ll.prepend(1)
-ll.print_list()
+# print(ll.remove(0).value)
+# ll.print_list()
 
-ll.pop_first()
-ll.print_list()
+# print(ll.remove(0))
+# ll.print_list()
 
-ll.pop_first()
-ll.print_list()
+# print(ll.insert(-1, 1))
+# ll.print_list()
 
-ll.pop_first()
-ll.print_list()
+# print(ll.insert(4, 1))
+# ll.print_list()
 
-ll.pop_first()
-ll.print_list()
+# print(ll.insert(3, 1))
+# ll.print_list()
+
+# print(ll.insert(0, 7))
+# ll.print_list()
+
+# print(ll.insert(2, 11))
+# ll.print_list()
+
+# print(ll.set_value(-1, 3))
+# print(ll.set_value(3, 2))
+# print(ll.set_value(0, 1))
+
+# ll.print_list()
+
+# print(ll.get(-1))
+# print(ll.get(3))
+# print(ll.get(0).value)
+# print(ll.get(1).value)
+# print(ll.get(2).value)
+
+
